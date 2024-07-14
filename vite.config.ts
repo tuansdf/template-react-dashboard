a14f178884d6cf42dc9a49acc31c37dd0dd4c55c
tuansdf <103062682+tuansdf@users.noreply.github.com>
2024-07-15 01:29:10 +0700

    update build

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,14 @@
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
-import crypto from "crypto";
-import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
-// to generate unique classnames
-const HASH_LENGTH = 16;
-const hash = (input: string) => {
-  return crypto.createHash("sha1").update(input).digest("hex").substring(0, HASH_LENGTH);
-};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
   css: {
     modules: {
-      generateScopedName: (name, filename) => {
-        const n = path.basename(filename) + name;
-        return `css-${hash(n)}`;
-      },
+      generateScopedName: "_[hash:base64:20]",
     },
   },
 });
